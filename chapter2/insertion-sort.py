aa9991bd8d093a53213a5eda7bd67bcadc9faa10
mohuanlanjing <mohuanlanjing@163.com>
2013-07-10 22:30:45 +0800

    * init finished

---
+++ chapter2/insertion-sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(wlist):
+    sorted_list = []
+    temp = 0
+    for i in wlist:
+        
+        
+if __name__ == "__main__":
+    l = [5,2,4,6,67,13,1]
+    print insertion_sort(l)
