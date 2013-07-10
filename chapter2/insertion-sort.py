9b881fb18d3c488aaeccbf5cbc85639836d38a5d
mohuanlanjing <mohuanlanjing@163.com>
2013-07-10 23:20:57 +0800

    * add reverse

--- chapter2/insertion-sort.py
+++ chapter2/insertion-sort.py
@@ -9,7 +9,20 @@ def insertion_sort(wlist):
             wlist[i] = temp
             i = i-1
     print wlist
+
+def insertion_reverse_sort(wlist):
+    temp = 0
+    for j in range(1, len(wlist)):
+        i = j-1
+        key = wlist[j]
+        while i>=0 and wlist[i] < key:
+            temp = wlist[i+1]
+            wlist[i+1] = wlist[i]
+            wlist[i] = temp
+            i = i - 1
+    print wlist
         
 if __name__ == "__main__":
     l = [5,2,4,6,67,13,1,0]
     insertion_sort(l)
+    insertion_reverse_sort(l)
